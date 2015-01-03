ecddbf94cb874200dd3c3b536dd371e05344e6de
Kristjan Korjus <korjus@gmail.com>
2015-01-03 11:45:21 +0200

    Skeleton for classification

--- classification.m
+++ classification.m
@@ -1,13 +1,23 @@
-function [ output_args ] = classification( input_args )
+function [ num_correct ] = classification( data_train, classes_train, ...
+  data_test, classes_test, parameters )
 %CLASSIFICATION Summary of this function goes here
 %   Detailed explanation goes here
 
 % this will be given hyper-parameters and it will classify
 % outputs number of correct classifications
 
-% PCA
+% If PCA, do PCA
+if parameters(1) == 2
+  
+end
 
 % Model
+if parameters(2) == 1
+  % Linear SVM
+  
+elseif parameters(2) == 2
+  % Decision t
+end
 
 % Number of correct
 
